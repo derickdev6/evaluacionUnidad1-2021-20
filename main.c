3bf5dc089b32a54712f69121d9415a6316193e8b
DerickPCLinux <derickdev6@gmail.com>
2021-08-28 16:03:42 -0500

    Moved functions to student.c and improved file system

--- main.c
+++ main.c
@@ -3,82 +3,33 @@
 #include <string.h>
 #include "student.h"
 
-void reverse(char *, int, int);
-
 int main(int argc, char *argv[])
 {
-    // Getting filenames as args
-
-    // Declaring files as NULL
-    FILE *inFile = NULL;
-    FILE *outFile = NULL;
-
-    // Checking for args
-    for (int i = 0; i < argc; i++)
+    if (argc != 3)
     {
-        printf("argv[%d]: %s\n", i, argv[i]);
-
-        // First arg will be taken as read
-        if (i == 1)
-        {
-            inFile = fopen(argv[i], "r");
-            if (inFile == NULL)
-            {
-                perror("Failed read file opening: ");
-                return (EXIT_FAILURE);
-            }
-        }
-
-        // Second arg will be taken as write
-        if (i == 2)
-        {
-            outFile = fopen(argv[i], "w");
-            if (outFile == NULL)
-            {
-                perror("Failed write file opening: ");
-                return (EXIT_FAILURE);
-            }
-        }
+        printf("Args must be 3");
+        return (EXIT_FAILURE);
     }
 
-    // Basic for reading lines
-    char buffer[70];
-    char *status = NULL;
-
-    // Feature 1 Read first inputFile line and write into outputFile
-    status = fgets(buffer, sizeof(buffer), inFile);
-    if (status != NULL)
+    FILE *fin = fopen(argv[1], "r");
+    if (fin == NULL)
     {
-        printf("%s", buffer);
-        fputs(buffer, outFile);
+        perror("Failed to open fin: ");
+        return (EXIT_FAILURE);
     }
-    // fclose(inFile);
-    // fclose(outFile);
 
-    // Feature 2 Read second line, reverse it and put it in O file
-    // status = fgets(buffer, sizeof(buffer), inFile);
-    status = fgets(buffer, sizeof(buffer), inFile);
-    if (status != NULL)
+    FILE *fout = fopen(argv[2], "a+");
+    if (fout == NULL)
     {
-        reverse(buffer, 0, strlen(buffer) - 1);
-        printf("%s", buffer);
-        fputs(buffer, outFile);
+        perror("Failed to open fout: ");
+        return (EXIT_FAILURE);
     }
-    fclose(inFile);
-    fclose(outFile);
+
+    feature1(fin, fout);
+    feature2(fin, fout);
+
+    fclose(fin);
+    fclose(fout);
 
     return EXIT_SUCCESS;
-}
-void reverse(char *x, int begin, int end)
-{
-    char c;
-
-    if (begin >= end)
-        return;
-
-    c = *(x + begin);
-    *(x + begin) = *(x + end);
-    *(x + end) = c;
-
-    reverse(x, ++begin, --end);
 }
--- student.c
+++ student.c
@@ -1,7 +1,53 @@
 #include <stdlib.h>
+#include <string.h>
 #include "student.h"
+#include <stdint.h>
 
-void feature1(FILE *fin, FILE *fout){
+// Declaring functions
+void reverse(char *, int, int);
 
-    
+void feature1(FILE *fin, FILE *fout);
+void feature2(FILE *fin, FILE *fout);
+
+void feature1(FILE *fin, FILE *fout)
+{
+    // Feature 1 Read first inputFile line and write into outputFile
+    // Basic for reading lines
+    char buffer[70];
+    char *status = NULL;
+
+    status = fgets(buffer, sizeof(buffer), fin);
+    if (status != NULL)
+    {
+        printf("%s", buffer);
+        fputs(buffer, fout);
+    }
+}
+
+void feature2(FILE *fin, FILE *fout)
+{
+    // Feature 2 Read second line, reverse and write it
+    char buffer[70];
+    char *status = NULL;
+
+    status = fgets(buffer, sizeof(buffer), fin);
+    if (status != NULL)
+    {
+        reverse(buffer, 0, strlen(buffer) - 1);
+        printf("%s", buffer);
+        fputs(buffer, fout);
+    }
+}
+void reverse(char *x, int begin, int end)
+{
+    char c;
+
+    if (begin >= end)
+        return;
+
+    c = *(x + begin);
+    *(x + begin) = *(x + end);
+    *(x + end) = c;
+
+    reverse(x, ++begin, --end);
 }
